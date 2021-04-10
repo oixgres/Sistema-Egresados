406ef27408ecb3ca286e68e8853442fd86227ac3
Sergio Enrique Guerra Cervantes <a1252819@uabc.edu.mx>
2021-04-09 17:50:37 -0700

    Trabajando en registro

--- php/insert.php
+++ php/insert.php
@@ -1,7 +1,37 @@
 <?php
-include_once 'dbh.php';
+require_once 'dbh.php';
 
-$first = $_POST['username'];
-$lastp = $_POST['password'];
+if($conn)
+{
+  $name = $_POST['idName'];
+  $lastname = $_POST['idLastName'];
+  $matricula = $_POST['idMat'];
+  $email = $_POST['idMail'];
+  $password = $_POST['idPass'];
+  $password2 = $_post['idPass2'];
+
+  $checkmail = mysqli_query($conn, "SELECT * FROM Usuario WHERE Correo='".$email."'");
+
+  if($checkmail)
+  {
+    echo "Correo ya existente";
+  }
+  else
+  {
+      if($password != $password)
+      {
+        echo "Error en Contraseña";
+      }
+      else
+      {
+          mysqli_query($conn, "INSERT INTO Usuario (Correo, Password, Nombres, Apellidos, Matricula) VALUES ('$email','$password','$name','$lastname','$matricula')");
+          header("Location: ../html/regSucessful.html");
+      }
+  }
+}
+else
+{
+  echo "No se pudo conectar con la base de datos";
+}
 
 ?>
