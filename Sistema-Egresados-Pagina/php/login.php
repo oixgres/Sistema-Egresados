2646b568a8364164a51807dfad1bf15e46e6d199
Sergio Enrique Guerra Cervantes <a1252819@uabc.edu.mx>
2021-04-13 18:53:01 -0700

    Generacion de cookies en el servidor y en el usuario

--- Sistema-Egresados-Pagina/php/login.php
+++ Sistema-Egresados-Pagina/php/login.php
@@ -1,5 +1,10 @@
-<?php
+ <?php
+session_start();
+//https://www.youtube.com/watch?v=3Gkx_8wU9Hw&ab_channel=ErickVladimirReyesMar%C3%ADnhttps://www.youtube.com/watch?v=3Gkx_8wU9Hw&ab_channel=ErickVladimirReyesMar%C3%ADn
+// Cerrar sesion 40:00
+
 require_once 'dbh.php';
+require_once 'generalFunctions.php';
 
 $mail = $_POST['userInput'];
 $pass = $_POST['passwordInput'];
@@ -8,12 +13,24 @@ $pass = $_POST['passwordInput'];
 $res = mysqli_query("SELECT * FROM Usuario WHERE Correo='".$mail."' AND Password='".$pass."'");
 $nr = mysqli_num_rows($nr);
 
+/* Si existe */
 if($nr == 1)
 {
+  /* Creamos un token para almacenarlo en cookies */
+  $token = sha1(uniqid(rand(10000000,99999999), true));
 
+  /* Guardamos el token en el servidor */
+  $_SESSION['token'] = $token;
+
+  /* Creamos una cookie para almacenar el token del lado del cliente*/
+  setcookie("token",$token,time()+(60*60*24*30), "/");
+  /* Creamos cookie para almacenar el nombre de cliente*/
+  setcookie("name", getFirstQueryElement($conn, "Usuario", "Nombre", "Correo", $mail));
+
+  header("Location: pruebas.php");
 }
 else
-  echo "Ha ocurrido un error, contacte con el administrador";
+  echo "Ha ocurrido un error, contacte con el administrador"; //si no existe
 
 
 
