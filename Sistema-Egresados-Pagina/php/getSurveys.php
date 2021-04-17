ea63f0b3f97656004af40dd945f4e335c21659bf
LeonardoTJ <leonardo.melendez@uabc.edu.mx>
2021-04-17 12:54:07 -0700

    Actualizacion a consulta para obtener encuestas.

--- Sistema-Egresados-Pagina/php/getSurveys.php
+++ Sistema-Egresados-Pagina/php/getSurveys.php
@@ -19,40 +19,63 @@ require_once "dbh.php";
  *     
  *  Códigos de error:
  *  -1 : No se mandó el idUsuario
- *  
- *  
+ *  -2 : No existe el idUsuario en la base de datos
+ *	-3 : No existen encuestas para este idUsuario
 */
 
 if(isset($_POST['idUsuario'])) {
-	$idUsuario = $_POST['idUsuario'];
+    $idUsuario = $_POST['idUsuario'];
 } else {
-	echo "-1";
+    echo -1;
+    $conn->close();
+    exit();
 }
+
+$sql = "SELECT idUsuario FROM Usuario WHERE idUsuario = '$idUsuario'";
+$res = mysqli_query($conn, $sql);
+
+if($res == 0) {
+    echo -2;
+    $conn->close();
+    exit();
+}
+
 $alcances = array("Universidad", "Campus", "Facultad", "Plan_Estudio");
 $json = array();
 
-for($i = 0; $i < 4; $i++) 
+for($i = 0; $i < 4; $i++)
 {
-    $sql = "SELECT idEncuesta, Encuesta.Nombre AS encuesta, '$alcances[$i]'.Nombre AS alcance FROM Encuesta
-            INNER JOIN Universidad ON Encuesta.'$alcances[$i]'_id'$alcances[$i]' = '$alcances[$i]'.id'$alcances[$i]'
-            INNER JOIN Datos_Escolares ON '$alcances[$i]'.id'$alcances[$i]' = Datos_Escolares.'$alcances[$i]'_id'$alcances[$i]'
+    $sql = "SELECT idEncuesta, Encuesta.Nombre AS encuesta, ${alcances[$i]}.Nombre AS alcance FROM Encuesta
+            INNER JOIN Universidad ON Encuesta.${alcances[$i]}_id${alcances[$i]} = ${alcances[$i]}.id${alcances[$i]}
+            INNER JOIN Datos_Escolares ON ${alcances[$i]}.id${alcances[$i]} = Datos_Escolares.${alcances[$i]}_id${alcances[$i]}
             INNER JOIN Usuario ON Datos_Escolares.Usuario_idUsuario = Usuario.idUsuario
             WHERE Usuario.idUsuario = '$idUsuario'";
 
     $res = mysqli_query($conn, $sql);
 
-    while($fila = mysqli_fetch_array($res)) {
-        $json [] = array(
-            'idEncuesta' => $fila['idEncuesta'],
-            'encuesta' => $fila['encuesta'],
-            'alcance' => $fila['alcance'],
-            'tipoAlcance' => $i
-        );
+    if(gettype($res) != "boolean"){
+        while($fila = mysqli_fetch_array($res)) {
+            $json [] = array(
+                'idEncuesta' => $fila['idEncuesta'],
+                'encuesta' => $fila['encuesta'],
+                'alcance' => $fila['alcance'],
+                'tipoAlcance' => $i
+            );
+        }
     }
 }
 
-$jsonString = json_encode($json); 	//convertir el json a cadena
-echo $jsonString; 					//retornar el json al frontend
+/*
+ *	JSON Ejemplo:
+ *	   string(84) "[{"idEncuesta":"3","encuesta":"Historial laboral","alcance":"UABC","tipoAlcance":0}]"
+*/
+
+if(!empty($json))
+{
+    $jsonString = json_encode($json); 	//convertir el json a cadena
+    echo $jsonString; 					//retornar el json al frontend
+} else {
+    echo -3;
+}
 
 $conn->close();
-?>
