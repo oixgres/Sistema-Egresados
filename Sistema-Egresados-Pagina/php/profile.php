d59954bf09041f062e4b46fe50499cd4f3223688
Sergio Enrique Guerra Cervantes <a1252819@uabc.edu.mx>
2021-04-16 19:37:26 -0700

    Detalles

--- Sistema-Egresados-Pagina/php/profile.php
+++ Sistema-Egresados-Pagina/php/profile.php
@@ -4,16 +4,33 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title><?php echo $_COOKIE["name"]."".$_COOKIE["lastname"] ?></title>
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/index.css">
   </head>
   <body>
-    <!-- Navbar -->
-    
+    <nav class="navbar navbar-collapse navbar-dark bg-dark">
+      <div class="container-fluid">
+
+      </div>
+      <a class="navbar-brand" href="profile.php">Sistema Egresados</a>
+
+      <ul class="navbar ml-auto justify-content-end">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+      </ul>
+      <a href="#" class="btn btn-outline-success">Salir</a>
     </nav>
     <?php if($_COOKIE["token"] == $_SESSION["token"]): ?>
-
+      <p><?php echo "Bienvenido ".$_COOKIE["name"]; ?></p>
     <?php else: ?>
-
+      <p><?php echo "Ocurrio un error"; ?></p>
     <?php endif; ?>
 
   </body>
