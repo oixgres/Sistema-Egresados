fc26f96e1a13c81542f594cd2c1540021640e96a
SolielLuimiere <54183914+SolielLumiere09@users.noreply.github.com>
2021-04-19 16:42:45 -0700

    Buscador de usuarios

--- Sistema-Egresados-Pagina/js/userFinder.js
+++ Sistema-Egresados-Pagina/js/userFinder.js
@@ -1,10 +1,35 @@
 $(document).ready(function (e) {
     const filters = $('.custom-checkbox');
+    const AdvanceSearch = $('#AdvanceSearch');
+    const FiltersContainer =  $('#FiltersContainer');
+    const OffCanvas = new bootstrap.Offcanvas($('#UserProfile')[0]);
 
     filters.parent().siblings().attr('disabled', true); //deshabilitar por default los filtros
     $('[data-toggle="tooltip"]').tooltip() //habilitar los tooltips
 
     filters.on('click', function (e) {
-        $(this).parent().siblings().attr('disabled', !$(this).prop('checked'))  //dependiendo el estado de los checkbox
+        $(this).parent().siblings().attr('disabled', !$(this).prop('checked'))
+    })
+
+    $('.showOffCanvas').on('click', function (e) {
+        e.stopPropagation();
+        OffCanvas.toggle();
+    })
+
+    FiltersContainer.hide();
+    FiltersContainer.removeClass('d-none');
+    
+    AdvanceSearch.on('click', function (e) {
+        const hideAdvanceSearch = 'Ocultar búsqueda avanzada';
+        const showAdvanceSearch = 'Búsqueda avanzada'
+        const FilterContainer = $('#FiltersContainer');
+
+        if($(this).text() === hideAdvanceSearch){
+            $(this).text(showAdvanceSearch)
+        }else{
+
+            $(this).text(hideAdvanceSearch)
+        }
+        FilterContainer.toggle(700);
     })
 })
