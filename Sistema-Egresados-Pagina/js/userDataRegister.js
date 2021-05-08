059edcb2d5dcbecdbd40a99218803cc74ebc4fde
SolielLuimiere <54183914+SolielLumiere09@users.noreply.github.com>
2021-05-08 16:49:37 -0700

    archivos acomodados

--- Sistema-Egresados-Pagina/js/userDataRegister.js
+++ Sistema-Egresados-Pagina/js/userDataRegister.js
@@ -1,8 +1,34 @@
 $(document).ready(function (e) {
     let stepper = new Stepper($('.bs-stepper')[0])
 
-    $('#button').on('click', function (e) {
+    const WINDOW_HEIGHT = $(window).height();;
 
-        stepper.next();
+    let questionContainerForIsWorking = $('#questionContainerForIsWorking');
+
+    $('.container').css({"height":`${WINDOW_HEIGHT}px`});
+
+    $("#isWorkingYes,#isWorkingNo").on('click', function (e) {
+        e.stopPropagation()
+        if($(this).attr('id') === "isWorkingYes"){
+            questionContainerForIsWorking.hide();
+            questionContainerForIsWorking.removeClass('d-none');
+            questionContainerForIsWorking.show(600);
+        }
+        else{
+            questionContainerForIsWorking.hide(600);
+        }
     })
+
+
+    if($("#isWorkingYes").prop('checked'))
+    {
+        questionContainerForIsWorking.hide();
+        questionContainerForIsWorking.removeClass('d-none');
+        questionContainerForIsWorking.show(600);
+    }
+
+    stepper.next();
+    stepper.next();
+
+
 })
