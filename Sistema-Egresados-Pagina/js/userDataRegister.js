8f648a434347e23ccfd2062cfb1dfb5e517a8ae3
SolielLuimiere <54183914+SolielLumiere09@users.noreply.github.com>
2021-05-08 19:09:12 -0700

    Añadido logica para guardar

--- Sistema-Egresados-Pagina/js/userDataRegister.js
+++ Sistema-Egresados-Pagina/js/userDataRegister.js
@@ -1,4 +1,24 @@
+function getCookie(cname) {
+    const name = cname + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+} //funcion para obtener un cookie
+
 $(document).ready(function (e) {
+    document.cookie ="id=1" //cookie temporal
+
+
+
     let stepper = new Stepper($('.bs-stepper')[0]) //stteper principal
     const WINDOW_HEIGHT = $(window).height(); //altura maxima del dispositivo
 
@@ -27,7 +47,12 @@ $(document).ready(function (e) {
 
     $('#PersonalDataBtn').on('click', function (e) {
         e.stopPropagation();
-        stepper.next();
+        const idUsuario = getCookie("id"); //obtener id de usuario
+        const fechaNacimiento = $('#Fecha_Nacimiento').val(); //fecha de nacimiento
+
+
+
+        //stepper.next();
     })
     $('#DatosEscolaresButton').on('click', function (e) {
         e.stopPropagation();
