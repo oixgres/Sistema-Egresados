c73d5f28d8e0f21355743a3372eec8ef73fe0753
Sergio Enrique Guerra Cervantes <60751647+oixgres@users.noreply.github.com>
2021-05-18 10:36:22 -0700

    Listo

--- Sistema-Egresados-Pagina/js/checkSession.js
+++ Sistema-Egresados-Pagina/js/checkSession.js
@@ -1,23 +1,22 @@
   
 function checkSession(userType){
-    $.ajax({
-        url: '../php/checkSession.php',
-        async: false,
-        type: 'POST',
-        data: {userType},
-        success: function (response) {
-            if(userType !== 'new')
-            {
-              console.log(response)
-              try{
-                  let json = JSON.parse(response)
-                  if(json.location){
-                      alert("Por favor verifique sus datos")
-                      window.location = json.location;
-                  }
-              }catch (e){
+  $.ajax({
+      url: '../php/checkSession.php',
+      async: false,
+      type: 'POST',
+      data: {userType},
+      success: function (response) {
+          if(userType != 'new')
+          {
+            console.log(response)
+            try{
+                let json = JSON.parse(response)
+                if(json.location){
+                    alert("Por favor verifique sus datos")
+                    window.location = json.location;
+                }
+            }catch (e){
 
-              }
             }
           }
           else
