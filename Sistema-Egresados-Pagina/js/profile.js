0811439ea0c81065b4419e46d1596919d87f7773
Sergio Enrique Guerra Cervantes <a1252819@uabc.edu.mx>
2021-05-18 10:29:06 -0700

    Intento de grid en el perfil

--- Sistema-Egresados-Pagina/js/profile.js
+++ Sistema-Egresados-Pagina/js/profile.js
@@ -1,4 +1,12 @@
+window.onload = () =>{
+  const grid = document.querySelector('.grid',{
+  });
+
+  const masonry = new Masonry(grid);
+}
+
 document.getElementById("profile-picture").onchange = function() {
     
     document.getElementById("profile-picture").submit();
 };
+
