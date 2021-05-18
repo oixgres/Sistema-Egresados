aaab81512cf7496264e372b0c3ce6a7fe9d2a2cc
Sergio Enrique Guerra Cervantes <a1252819@uabc.edu.mx>
2021-05-18 10:40:55 -0700

    Correcion

--- Sistema-Egresados-Pagina/js/checkSession.js
+++ Sistema-Egresados-Pagina/js/checkSession.js
@@ -1,22 +1,21 @@
 function checkSession(userType){
-    $.ajax({
-        url: '../php/checkSession.php',
-        async: false,
-        type: 'POST',
-        data: {userType},
-        success: function (response) {
-            if(userType !== 'new')
-            {
-              console.log(response)
-              try{
-                  let json = JSON.parse(response)
-                  if(json.location){
-                      alert("Por favor verifique sus datos")
-                      window.location = json.location;
-                  }
-              }catch (e){
+  $.ajax({
+      url: '../php/checkSession.php',
+      async: false,
+      type: 'POST',
+      data: {userType},
+      success: function (response) {
+          if(userType != 'new')
+          {
+            console.log(response)
+            try{
+                let json = JSON.parse(response)
+                if(json.location){
+                    alert("Por favor verifique sus datos")
+                    window.location = json.location;
+                }
+            }catch (e){
 
-              }
             }
             else
               try {
@@ -29,3 +28,4 @@ function checkSession(userType){
         }
     })
 }
+
