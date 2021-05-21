c9dda6703da1f058b92c0d5637d284e44b4c5264
Victor Manuel Vera Arias <54183970+VictorVeraA@users.noreply.github.com>
2021-05-21 13:32:30 -0700

    Caracteres UTF-8 validos en querys

--- Sistema-Egresados-Pagina/php/dbh.php
+++ Sistema-Egresados-Pagina/php/dbh.php
@@ -7,21 +7,21 @@ $dbName = "sistemaegresados";
 */
 
 
-
+/*
 $dbServername = "localhost";
 $dbUsername = "root";
 $dbPassword = "harry6627";
 $dbName = "sistemaegresados";
-
+*/
 
 /* Servidor Actual */
 
-/*
+
 $dbServername = "localhost";
 $dbUsername = "omdprofx_guerra-c";
 $dbPassword = "Gue1870";
 $dbName = "omdprofx_guerra-c-ordinario";
-*/
+
 
 /*
 $dbServername = "localhost";
@@ -31,5 +31,9 @@ $dbName = "omdprofx_omarmontoya-proyecto";
 */
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+// Habilita caracteres con acento para busquedas.
+
+$conn->set_charset("utf8");
 
 ?>
