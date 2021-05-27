95564ec2ba093ee8772cce5f2260ededc8ed3a46
Leonardo Melendez <leonardo.melendez@uabc.edu.mx>
2021-05-26 23:31:03 -0700

    Fix identificadores en PHP userAnswers

--- Sistema-Egresados-Pagina/php/getUserAnswers.php
+++ Sistema-Egresados-Pagina/php/getUserAnswers.php
@@ -41,7 +41,7 @@ if(isset($_POST['idUsuario'])) {
 
 //Si la encuesta existe...
 
-$sql = "SELECT * FROM encuesta WHERE idEncuesta = ${idEncuesta}";
+$sql = "SELECT * FROM Encuesta WHERE idEncuesta = ${idEncuesta}";
 $res = mysqli_query($conn, $sql);
 
 if($res->num_rows == 0) {
@@ -52,7 +52,7 @@ if($res->num_rows == 0) {
 
 //Si la encuesta está contestada...
 
-$sql = "SELECT * FROM encuestas_contestadas 
+$sql = "SELECT * FROM Encuestas_Contestadas 
 		WHERE Encuesta_idEncuesta = ${idEncuesta} 
 		AND Usuario_idUsuario = ${idUsuario}";
 $res = mysqli_query($conn, $sql);
@@ -65,7 +65,7 @@ if($res->num_rows == 0) {
 
 //Verificamos que haya preguntas en la encuesta...
 
-$sql = "SELECT Pregunta FROM pregunta
+$sql = "SELECT Pregunta FROM Pregunta
 		WHERE Encuesta_idEncuesta = ${idEncuesta}
 		ORDER BY idPregunta";
 $res = mysqli_query($conn, $sql);
@@ -78,11 +78,11 @@ if($res->num_rows == 0) {
 
 //Obtenemos todas las respuestas del usuario...
 
-$sql = "SELECT pregunta.Pregunta, respuesta_usuario.Respuesta FROM respuesta_usuario
-		INNER JOIN pregunta ON pregunta.idPregunta = respuesta_usuario.Pregunta_idPregunta
-		INNER JOIN encuesta ON encuesta.idEncuesta = pregunta.Encuesta_idEncuesta
-		WHERE encuesta.idEncuesta = ${idEncuesta}
-		ORDER BY pregunta.idPregunta";
+$sql = "SELECT Pregunta.Pregunta, Respuesta_Usuario.Respuesta FROM Respuesta_Usuario
+		INNER JOIN Pregunta ON Pregunta.idPregunta = Respuesta_Usuario.Pregunta_idPregunta
+		INNER JOIN Encuesta ON Encuesta.idEncuesta = Pregunta.Encuesta_idEncuesta
+		WHERE Encuesta.idEncuesta = ${idEncuesta}
+		ORDER BY Pregunta.idPregunta";
 $respuestas_usuario = mysqli_query($conn, $sql);
 
 if($respuestas_usuario->num_rows == 0) {
