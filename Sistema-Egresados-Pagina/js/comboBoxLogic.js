6f80dbccc716b27b46a36a7363f8490fb2c3900b
SolielLuimiere <54183914+SolielLumiere09@users.noreply.github.com>
2021-05-26 22:20:36 -0700

    listas terminadas

---
+++ Sistema-Egresados-Pagina/js/comboBoxLogic.js
@@ -0,0 +1,9 @@
+$(document).ready(function (e) {
+    function optionComponent(text){
+        //<option></option>
+        return $(`
+            <option>text</option>    
+        `)
+    }
+
+})
