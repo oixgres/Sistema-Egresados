53dbe3cba46f0233048f674665a18f52e134edcc
Victor Vera <54183970+VictorVeraA@users.noreply.github.com>
2021-05-31 21:22:03 -0700

    Update dbh.php

--- Sistema-Egresados-Pagina/php/dbh.php
+++ Sistema-Egresados-Pagina/php/dbh.php
@@ -1,43 +1,9 @@
-
 <?php
-/*
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "harry1234";
-$dbName = "sistemaegresados";
-*/
 
-
-
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "harry6627";
-$dbName = "sistemaegresados";
-
-
-/* Servidor Actual */
-/*
-$dbServername = "localhost";
-$dbUsername = "omdprofx_guerra-c";
-$dbPassword = "Gue1870";
-$dbName = "omdprofx_guerra-c-ordinario";
-*/
-
-/*
-$dbServername = "localhost";
-$dbUsername = "omdprofx_omarmontoya";
-$dbPassword = "Oma46012";
-$dbName = "omdprofx_omarmontoya-proyecto";
-*/
-
-
-/* Servidor de Reyes */
-/*
 $dbServername = 'localhost';
 $dbUsername = 'conisoft_SE';
 $dbPassword = 'Portal071198';
 $dbName = 'conisoft_Sistema-Egresados';
-*/
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
